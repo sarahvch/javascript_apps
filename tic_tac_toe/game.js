d382bc59e3c1d91304ce7d718b4f7bfd89912aee
sarahvch <sarahvch@gmail.com>
2017-06-20 11:15:22 -0600

    tictactoe

---
+++ tic_tac_toe/game.js
@@ -0,0 +1,203 @@
+var boxOne = document.querySelector('#one')
+var boxTwo = document.querySelector('#two')
+var boxThree = document.querySelector('#three')
+var boxFour = document.querySelector('#four')
+var boxFive = document.querySelector('#five')
+var boxSix = document.querySelector('#six')
+var boxSeven = document.querySelector('#seven')
+var boxEight = document.querySelector('#eight')
+var boxNine = document.querySelector('#nine')
+
+
+//mouse over
+boxOne.addEventListener('mouseover',function(){
+  boxOne.style.backgroundColor = '#80e5ff';
+})
+boxTwo.addEventListener('mouseover',function(){
+  boxTwo.style.backgroundColor = '#ACACDE';
+})
+boxThree.addEventListener('mouseover',function(){
+  boxThree.style.backgroundColor = '#99ffbb';
+})
+boxFour.addEventListener('mouseover',function(){
+  boxFour.style.backgroundColor = '#ffff00';
+})
+boxFive.addEventListener('mouseover',function(){
+  boxFive.style.backgroundColor = '#00e64d';
+})
+boxSix.addEventListener('mouseover',function(){
+  boxSix.style.backgroundColor = '#ffcc00';
+})
+boxSeven.addEventListener('mouseover',function(){
+  boxSeven.style.backgroundColor = '#b3ffd9';
+})
+boxEight.addEventListener('mouseover',function(){
+  boxEight.style.backgroundColor = '#66e0ff';
+})
+boxNine.addEventListener('mouseover',function(){
+  boxNine.style.backgroundColor = '#9933ff';
+})
+
+
+
+//mouse out
+boxOne.addEventListener('mouseout', function(){
+  boxOne.style.backgroundColor = '#1E91D6';
+})
+boxTwo.addEventListener('mouseout', function(){
+  boxTwo.style.backgroundColor = '#B8336A';
+})
+boxThree.addEventListener('mouseout', function(){
+  boxThree.style.backgroundColor = '#8FC93A';
+})
+boxFour.addEventListener('mouseout', function(){
+  boxFour.style.backgroundColor = '#E4CC37';
+})
+boxFive.addEventListener('mouseout', function(){
+  boxFive.style.backgroundColor = '#009933';
+})
+boxSix.addEventListener('mouseout', function(){
+  boxSix.style.backgroundColor = '#E18335';
+})
+boxSeven.addEventListener('mouseout', function(){
+  boxSeven.style.backgroundColor = '#78D5D7';
+})
+boxEight.addEventListener('mouseout', function(){
+  boxEight.style.backgroundColor = '#037971';
+})
+boxNine.addEventListener('mouseout', function(){
+  boxNine.style.backgroundColor = '#274690';
+})
+
+//clicked on
+boxOne.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+
+boxTwo.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+
+boxThree.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+
+boxFour.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxFive.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxSix.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxSeven.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxEight.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxNine.addEventListener('click', function(){
+  this.textContent = "X";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+
+//double click
+boxOne.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxTwo.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxThree.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxFour.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxFive.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxSix.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxSeven.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxEight.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
+boxNine.addEventListener('dblclick', function(){
+  this.textContent = "O";
+  this.style.fontSize = '100px'
+  this.style.color = "#eee";
+  this.style.textAlign = "center";
+  this.style.font = "arial";
+})
