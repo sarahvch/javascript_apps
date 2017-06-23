bee9fcb3a00938532e1330cb59353812b63b5352
sarahvch <sarahvch@gmail.com>
2017-06-23 15:49:06 -0600

    refactoring

--- connect_four/game.js
+++ connect_four/game.js
@@ -9,6 +9,57 @@ tog = 2
 
 var tableData = $('.stylish')
 
+function redBlue(colNum){
+  if(tog%2 == 0){
+    tableData.eq([colNum]).css('background-color','#c2f0f0')
+  }else{
+    tableData.eq([colNum]).css('background-color','#ff8080')
+  }
+}
+
+
+$('#1').click(function() {
+  redBlue(colOne)
+  tog = tog + 1
+  colOne = colOne-7
+})
+
+$('#2').click(function() {
+  redBlue(colTwo)
+  tog = tog + 1
+  colTwo = colTwo-7
+})
+
+$('#3').click(function() {
+  redBlue(colThree)
+  tog = tog + 1
+  colThree = colThree-7
+})
+
+$('#4').click(function() {
+  redBlue(colFour)
+  tog = tog + 1
+  colFour = colFour-7
+})
+
+$('#5').click(function() {
+  redBlue(colFive)
+  tog = tog + 1
+  colFive = colFive-7
+})
+
+$('#6').click(function() {
+  redBlue(colSix)
+  tog = tog + 1
+  colSix = colSix-7
+})
+
+$('#7').click(function() {
+  redBlue(colSeven)
+  tog = tog + 1
+  colSeven = colSeven-7
+})
+
 //REFACTORING NOTES
 // var topRow = $(".first td a[id]")
 // var topRowID = topRow.map(function (){return this.id});
@@ -44,55 +95,3 @@ var tableData = $('.stylish')
 //   colTwo = colTwo - 7
 //   tog = tog + 1
 // })
-
-
-function cols(colNum){
-  if(tog%2 == 0){
-    tableData.eq([colNum]).css('background-color','#c2f0f0')
-  }else{
-    tableData.eq([colNum]).css('background-color','#ff8080')
-  }
-}
-
-
-$('#1').click(function() {
-  cols(colOne)
-  tog = tog + 1
-  colOne = colOne-7
-})
-
-$('#2').click(function() {
-  cols(colTwo)
-  tog = tog + 1
-  colTwo = colTwo-7
-})
-
-$('#3').click(function() {
-  cols(colThree)
-  tog = tog + 1
-  colThree = colThree-7
-})
-
-$('#4').click(function() {
-  cols(colFour)
-  tog = tog + 1
-  colFour = colFour-7
-})
-
-$('#5').click(function() {
-  cols(colFive)
-  tog = tog + 1
-  colFive = colFive-7
-})
-
-$('#6').click(function() {
-  cols(colSix)
-  tog = tog + 1
-  colSix = colSix-7
-})
-
-$('#7').click(function() {
-  cols(colSeven)
-  tog = tog + 1
-  colSeven = colSeven-7
-})
